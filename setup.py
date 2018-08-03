683140dc44fb1f055d97d558d487772a4ec36373
Pete Fein <pete@syste.ma>
2018-08-03 17:42:47 -0400

    build directly with Cython

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 from setuptools import setup, Extension, find_packages
+from Cython.Build import cythonize
 import numpy
 
-ext_modules = [Extension('mdr._tree',
-    sources=['mdr/_tree.c'],
-    include_dirs = [numpy.get_include()],
-)]
 
 setup(name='mdr',
       version='0.0.1',
@@ -15,6 +12,8 @@ setup(name='mdr',
       url='https://github.com/tpeng/mdr',
       license='MIT',
       packages=find_packages(exclude=['tests', 'tests.*']),
-      ext_modules=ext_modules,
-      install_requires=['lxml', 'numpy', 'scipy']
+      ext_modules = cythonize('mdr/_tree.pyx'),
+      include_dirs = [numpy.get_include()],
+      install_requires=['lxml', 'numpy', 'scipy', 'Cython'],
+      zip_safe=False,
 )
